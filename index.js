f1bf749ef8a4966c2eb2bffe982c2866d9860f3e
Orrokat <susan_a_regan@yahoo.com>
2019-01-24 10:19:25 -0500

    add function to cycle frequencies to find duplicates.  Includes cleanup of files.

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
-//app.use(express.static('files'));
-
 
 app.get('/', function (req, res) {
-    fs.readFile("day1a.csv", "utf8", function(err, data){
+    fs.readFile("files\\day1a.csv", "utf8", function(err, data){
         if(err) throw err;
-        res.send(`${dayOne.adjust(data.split(/\r\n|\n|\r/gm))}`)
+        res.send(`${"DAY ONE FIRST PART - Total Frequency = " + dayOne.adjust(data.split(/\r\n|\n|\r/gm))}`)
+ 
+    });
+});
+
+app.get('/day1b', function (req, res) {
+    fs.readFile("files\\day1a.csv", "utf8", function(err, data){
+        if(err) throw err;
+        res.send(`${"DAY ONE SECOND PART - First Duplicate Found = " + dayOne.cycleFrequencyChanges(data.split(/\r\n|\n|\r/gm), 0, [])}`)
+ 
     });
 });
 
