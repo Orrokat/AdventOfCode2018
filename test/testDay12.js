65967f24f2a4d1a3783c0570cbb68355bf99dc00
Orrokat <susan.a.regan@gmail.com>
2019-09-24 18:50:36 -0400

    Begin day 12 and lint

---
+++ test/testDay12.js
@@ -0,0 +1,58 @@
+var assert = require('assert')
+var day12 = require('../lib/day12.js')
+
+describe('day12', function () {
+  describe('day12.extractRulesArray - given an array of rules for plant expansion  ', function () {
+    it('should return an array of the rules that result in a live plant', function () {
+      var plantExpansionRules =
+      [ '...## => #',
+        '..... => .',
+        '..#.. => #',
+        '##### => .',
+        '.#... => #',
+        '.#.#. => #',
+        '.#.## => #',
+        '.##.. => #',
+        '.#### => #',
+        '....# => .',
+        '#.... => .',
+        '#.#.# => #',
+        '#.### => #',
+        '##.#. => #',
+        '##.## => #',
+        '###.. => #',
+        '###.# => #',
+        '####. => #' ]
+
+      var rulesArray = day12.extractRulesArray(plantExpansionRules)
+
+      var expectedArray =
+      [ '...##',
+        '..#..',
+        '.#...',
+        '.#.#.',
+        '.#.##',
+        '.##..',
+        '.####',
+        '#.#.#',
+        '#.###',
+        '##.#.',
+        '##.##',
+        '###..',
+        '###.#',
+        '####.' ]
+      assert.strict.deepEqual(rulesArray, expectedArray)
+    })
+  })
+  describe('day12.initializeGenerationArray - given a string representing the initial state of the plants in each pot', function () {
+    it('should return an array of the initial state of the plants, including 2 empty pots beyond each end', function () {
+      var initialPlants = 'initial state: #..#.#..##......###...###'
+      var generationArray = day12.initializeGenerationArray(initialPlants)
+
+      var expectedArray = [
+        '.', '.', '#', '.', '.', '#', '.', '#', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.'
+      ]
+      assert.strict.deepEqual(generationArray, expectedArray)
+    })
+  })
+})
--- test/testDayEight.js
+++ test/testDayEight.js
@@ -1,41 +1,34 @@
-var assert = require('assert');
-var dayEight = require("../lib/dayEight.js");
+var assert = require('assert')
+var dayEight = require('../lib/dayEight.js')
 
 describe('dayEight', function () {
+  describe('dayEight.sumMetaData - given an array of numbers  ', function () {
+    it('should return a sum of the metadata', function () {
+      var licenseNodeDataArray = [0, 3, 10, 11, 12]
+      var metaDataSum = dayEight.sumMetaData(licenseNodeDataArray)
 
-    describe('dayEight.sumMetaData - given an array of numbers  ', function () {
-        it('should return a sum of the metadata', function () {
-            var licenseNodeDataArray = [0, 3, 10, 11, 12];
-            var nodeIndex = 0;
-            var runningSum = 0;
-            var arrayIndex = 0;
-            var metaDataSum = dayEight.sumMetaData(licenseNodeDataArray);
+      var expectedSum = 33
+      assert.strictEqual(metaDataSum, expectedSum)
+    })
+  })
 
-            var expectedSum = 33;
-            assert.equal(metaDataSum, expectedSum);
+  describe('dayEight.sumMetaData - given an array of numbers  ', function () {
+    it('should return a sum of the metadata', function () {
+      var licenseNodeDataArray = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+      var metaDataSum = dayEight.sumMetaData(licenseNodeDataArray)
 
-        });
-    });
+      var expectedSum = 138
+      assert.strictEqual(metaDataSum, expectedSum)
+    })
+  })
 
-    describe('dayEight.sumMetaData - given an array of numbers  ', function () {
-        it('should return a sum of the metadata', function () {
-            var licenseNodeDataArray = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
-            var metaDataSum = dayEight.sumMetaData(licenseNodeDataArray);
+  describe('dayEight.sumTree - given an array of numbers  ', function () {
+    it('should return a sum of the primary node', function () {
+      var licenseNodeDataArray = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+      var metaDataSum = dayEight.sumTree(licenseNodeDataArray)
 
-            var expectedSum = 138;
-            assert.equal(metaDataSum, expectedSum);
-
-        });
-    });
-
-    describe('dayEight.sumTree - given an array of numbers  ', function () {
-        it('should return a sum of the primary node', function () {
-            var licenseNodeDataArray = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
-            var metaDataSum = dayEight.sumTree(licenseNodeDataArray);
-
-            var expectedSum = 66;
-            assert.equal(metaDataSum, expectedSum);
-
-        });
-    });
-});
+      var expectedSum = 66
+      assert.strictEqual(metaDataSum, expectedSum)
+    })
+  })
+})
