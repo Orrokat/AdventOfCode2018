22bcec32fa2435c85fcd91abad964482abc5c4a9
Orrokat <susan_a_regan@yahoo.com>
2019-10-07 13:47:28 -0400

    Split day12 plant array

--- test/testDay12.js
+++ test/testDay12.js
@@ -45,12 +45,23 @@ describe('day12', function () {
     })
   })
   describe('day12.initializeGenerationArray - given a string representing the initial state of the plants in each pot', function () {
-    it('should return an array of the initial state of the plants, including 2 empty pots beyond each end', function () {
+    it('should return an array of the initial state of the plants, including 4 empty pots beyond each end', function () {
       var initialPlants = 'initial state: #..#.#..##......###...###'
       var generationArray = day12.initializeGenerationArray(initialPlants)
 
       var expectedArray = [
-        '.', '.', '#', '.', '.', '#', '.', '#', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.'
+        '.', '.', '.', '.', '#', '.', '.', '#', '.', '#', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '.'
+      ]
+      assert.strict.deepEqual(generationArray, expectedArray)
+    })
+  })
+  describe('day12.initializeGenerationArray - given a string representing the initial state of the plants in each pot', function () {
+    it('should return an array of the initial state of the plants, including 4 empty pots beyond each end', function () {
+      var initialPlants = 'initial state: #..#.#..##......###...###'
+      var generationArray = day12.initializeGenerationArray(initialPlants)
+
+      var expectedArray = [
+        '.', '.', '.', '.', '#', '.', '.', '#', '.', '#', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '.'
       ]
       assert.strict.deepEqual(generationArray, expectedArray)
     })
