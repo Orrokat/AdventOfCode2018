5d1062f256bf77bd3423bf1e6cd75b94aefb91ef
Orrokat <susan.a.regan@gmail.com>
2019-10-07 15:06:34 -0400

    Determine Live plants

--- test/testDay12.js
+++ test/testDay12.js
@@ -55,15 +55,61 @@ describe('day12', function () {
       assert.strict.deepEqual(generationArray, expectedArray)
     })
   })
-  describe('day12.initializeGenerationArray - given a string representing the initial state of the plants in each pot', function () {
-    it('should return an array of the initial state of the plants, including 4 empty pots beyond each end', function () {
-      var initialPlants = 'initial state: #..#.#..##......###...###'
-      var generationArray = day12.initializeGenerationArray(initialPlants)
+  describe('day12.doesThisPlantLive - given a string representing the current state of a single pot and 2 pots each direction from it and an array of rules', function () {
+    it('should return a boolean indicating if this plant lives in the next generation', function () {
+      var plantExpansionRules =
+      [ '...## => #',
+        '..... => .',
+        '..#.. => #',
+        '##### => .',
+        '.#... => #',
+        '.#.#. => #',
+        '.#.## => #',
+        '.##.. => #',
+        '.#### => #',
+        '....# => .',
+        '#.... => .',
+        '#.#.# => #',
+        '#.### => #',
+        '##.#. => #',
+        '##.## => #',
+        '###.. => #',
+        '###.# => #',
+        '####. => #' ]
 
-      var expectedArray = [
-        '.', '.', '.', '.', '#', '.', '.', '#', '.', '#', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '.'
-      ]
-      assert.strict.deepEqual(generationArray, expectedArray)
+      var rulesArray = day12.extractRulesArray(plantExpansionRules)
+      var thisPlantLives = day12.doesThisPlantLive('...##', rulesArray)
+      
+      assert.strict.deepEqual(thisPlantLives, true)
+    })
+  })
+  describe('day12.doesThisPlantLive - given a string representing the current state of a single pot and 2 pots each direction from it and an array of rules', function () {
+    it('should return a boolean indicating if this plant lives in the next generation', function () {
+      var plantExpansionRules =
+      [ '...## => #',
+        '..... => .',
+        '..#.. => #',
+        '##### => .',
+        '.#... => #',
+        '.#.#. => #',
+        '.#.## => #',
+        '.##.. => #',
+        '.#### => #',
+        '....# => .',
+        '#.... => .',
+        '#.#.# => #',
+        '#.### => #',
+        '##.#. => #',
+        '##.## => #',
+        '###.. => #',
+        '###.# => #',
+        '####. => #' ]
+
+      
+      var rulesArray = day12.extractRulesArray(plantExpansionRules)
+      var thisPlantLives = day12.doesThisPlantLive('.....', rulesArray)
+      
+      assert.strict.deepEqual(thisPlantLives, false)
     })
   })
 })
